e31865bc19a8791bcbb9d0b13e96b2a42a54d3bb
jan28f <163349875+jan28f@users.noreply.github.com>
2024-10-26 00:06:32 -0300

    Implementacion de extension clase ObjectGame

--- core/src/main/java/puppy/code/Block.java
+++ core/src/main/java/puppy/code/Block.java
@@ -1,28 +1,38 @@
 package puppy.code;
 
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
-
 import java.util.Random;
-
 import com.badlogic.gdx.graphics.Color;
 
-public class Block {
-    int x,y,width,height;
+/**
+ * Clase Block extendida de ObjectGame, representa un bloque del juego.
+ */
+public class Block extends ObjectGame
+{
     Color cc;
     boolean destroyed;
-    
-    public Block(int x, int y, int width, int height) {
-        this.x = x;
-        this.y = y;
-        this.width = width;
-        this.height = height;
+
+    /**
+     * Constructor de la clase.
+     * @param x Un numero entero, representa la posicion del objeto en el eje x.
+     * @param y Un numero entero, representa la posicion del objeto en el eje y.
+     * @param width Un numero entero, representa el ancho del objeto.
+     * @param height Un numero entero, representa el alto del objeto.
+     */
+    public Block(int x, int y, int width, int height)
+    {
+        super(x, y, width, height);
         destroyed = false;
         Random r = new Random(x+y);
-        
-       cc = new Color(0.1f+r.nextFloat(1), r.nextFloat(1), r.nextFloat(1), 10);
-  
+        cc = new Color(0.1f+r.nextFloat(1), r.nextFloat(1), r.nextFloat(1), 10);
     }
-    public void draw(ShapeRenderer shape){
+
+    /**
+     * Implementacion del metodo abstracto heredado draw, se dibuja el bloque.
+     * @param shape Variable que representa el bloque a dibujar.
+     */
+    public void draw(ShapeRenderer shape)
+    {
     	shape.setColor(cc);
         shape.rect(x, y, width, height);
     }
--- core/src/main/java/puppy/code/Paddle.java
+++ core/src/main/java/puppy/code/Paddle.java
@@ -5,35 +5,39 @@ import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 
-public class Paddle {
-    private int x = 20;
-    private int y = 20;
-    private int width = 100;
-    private int height = 10;
-    
+/**
+ * Clase Paddle extendida de ObjectGame, representa la paleta del juego.
+ */
+public class Paddle extends ObjectGame
+{
+    /**
+     * Constructor de la clase.
+     * @param x Un numero entero, representa la posicion del objeto en el eje x.
+     * @param y Un numero entero, representa la posicion del objeto en el eje y.
+     * @param ancho Un numero entero, representa el ancho del objeto.
+     * @param alto Un numero entero, representa el alto del objeto.
+     */
     public Paddle(int x, int y, int ancho, int alto) {
-    	this.x = x;
-    	this.y= y;
-    	width = ancho;
-    	height = alto;
+    	super(x, y, ancho, alto);
     }
-     
-    public int getX() {return x;}
-	public int getY() {return y;}
-	public int getWidth() {return width;}
-	public int getHeight() {return height;}
 
-	public void draw(ShapeRenderer shape){
+    /**
+     * Implementacion del metodo abstracto heredado draw, se dibuja la paleta.
+     * @param shape Variable que representa la paleta a dibujar.
+     */
+	public void draw(ShapeRenderer shape)
+    {
         shape.setColor(Color.BLUE);
         int x2 = x; //= Gdx.input.getX();
-        if (Gdx.input.isKeyPressed(Input.Keys.LEFT) || Gdx.input.isKeyPressed(Input.Keys.A)) x2 =x-8;
-        if (Gdx.input.isKeyPressed(Input.Keys.RIGHT) || Gdx.input.isKeyPressed(Input.Keys.D)) x2=x+8;
-       // y = Gdx.graphics.getHeight() - Gdx.input.getY(); 
+
+        if (Gdx.input.isKeyPressed(Input.Keys.LEFT) || Gdx.input.isKeyPressed(Input.Keys.A))
+            x2 =x-8;
+        if (Gdx.input.isKeyPressed(Input.Keys.RIGHT) || Gdx.input.isKeyPressed(Input.Keys.D))
+            x2=x+8;
+        // y = Gdx.graphics.getHeight() - Gdx.input.getY();
         if (x2 > 0 && x2+width < Gdx.graphics.getWidth()) {
             x = x2;
         }
         shape.rect(x, y, width, height);
     }
-    
-    
 }
