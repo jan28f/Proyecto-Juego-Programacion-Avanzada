ce4756f9a464b84ec10cf4ac9ad22819ffb91113
jan28f <163349875+jan28f@users.noreply.github.com>
2024-10-27 21:10:50 -0300

    Implementacion de documentacion

--- core/src/main/java/puppy/code/Colisionable.java
+++ core/src/main/java/puppy/code/Colisionable.java
@@ -2,6 +2,16 @@ package puppy.code;
 
 public interface Colisionable
 {
-    public boolean revisarColision(PingBall pelota);
+    /**
+     * Se verifica si existe la colision entre un elemento y la pelota.
+     * @param pelota Pelota a verificar si colisiona con el elemento.
+     * @return true si existe colision entre los elementos, false en caso contrario.
+     */
+    public boolean verificarColision(PingBall pelota);
+
+    /**
+     * Se verifica y se realiza una accion en case de existir colision con una pelota.
+     * @param pelota Pelota a verificar si colisiona con el elemento.
+     */
     public void colisionaCon(PingBall pelota);
 }
