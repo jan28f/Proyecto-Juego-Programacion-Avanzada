a56758e0ccec57050071955b78716362e0afdb75
Jose Mena <163463282+TheFanking@users.noreply.github.com>
2024-11-23 00:03:44 -0300

    Update AumentoDurabilidad.java
    Faltaba aplicar la clase random para porder hacer que los aumentos de durabilidad sean aleatorio

--- core/src/main/java/puppy/code/AumentoDurabilidad.java
+++ core/src/main/java/puppy/code/AumentoDurabilidad.java
@@ -2,6 +2,7 @@ package puppy.code;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public class AumentoDurabilidad implements CambioBloques{
     @Override
@@ -9,8 +10,9 @@ public class AumentoDurabilidad implements CambioBloques{
     {
         for (int i = 0; i < bloques.size(); i++)
         {
-            bloques.get(i).setDurabilidad(bloques.get(i).getDurabilidad() + 1);
+            Random rand = new Random();
+            int random = rand.nextInt(bloques.size());
+            bloques.get(random).setDurabilidad(bloques.get(random).getDurabilidad() + 1);
         }
     }
 }
-
